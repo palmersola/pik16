73eb52aec51b315a3a5fa34070fb503b61326cb7
Stephen Alfa <stojalfa@gmail.com>
2023-08-03 09:58:43 -0400

    Integration

---
+++ client/pik16/src/pages/GameCard/GameCard.js
@@ -0,0 +1,24 @@
+// GameCard.js
+import React from 'react';
+
+const GameCard = ({ game }) => {
+    return (
+        <div className='row m-3'>
+            <div className="card col-sm-12">
+                <div className="d-flex justify-content-between align-items-center">
+                    <div className="team-info text-center">
+                        <img className="team-logo" src={game.away.logos[0]} alt="Away Team Logo" />
+                        <h5 className="team-name">{game.away.abbreviation}</h5>
+                    </div>
+                    <div className="vs">vs</div>
+                    <div className="team-info text-center">
+                        <img className="team-logo" src={game.home.logos[0]} alt="Home Team Logo" />
+                        <h5 className="team-name">{game.home.abbreviation}</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default GameCard;
