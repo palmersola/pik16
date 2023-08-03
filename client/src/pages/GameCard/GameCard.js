5715aa19addaf566786b9d41933f629cd089939d
palmersola <palmersola@yahoo.com>
2023-08-03 16:00:14 -0500

    Changes by stephenalfa

--- client/src/pages/GameCard/GameCard.js
+++ client/src/pages/GameCard/GameCard.js
@@ -2,23 +2,33 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
 
-const GameCard = ({ game }) => {
+const GameCard = () => {
+    const [gamesArr, setGamesArr] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:8080/api/football/games').then((response) => {
+            setGamesArr(response.data);
+        });
+    }, []);
+
     return (
 
-
-        <div className='row m-3'>
-            <div className="card col-sm-12">
-                <div className="d-flex justify-content-between align-items-center">
-                    <div className="team-info text-center">
-                        <img className="team-logo" src={game.away.logos[0]} alt="Away Team Logo" />
-                        <h5 className="team-name">{game.away.abbreviation}</h5>
+        <div className="container mt-5">
+            <h1>Game Feed</h1>
+            <div className="row">
+                {gamesArr.map((game, index) => (
+                    <div key={index} id={game.game.id} className="card m-3 p-3 col-sm-5 d-flex flex-row justify-content-between align-items-center">
+                        <div className="team-info text-center">
+                            <img className="team-logo" src={game.away.logos[0]} alt="Away Team Logo" />
+                            <h5 className="team-name">{game.away.school}</h5>
+                        </div>
+                        <div className="vs">vs</div>
+                        <div className="team-info text-center">
+                            <img className="team-logo" src={game.home.logos[0]} alt="Home Team Logo" />
+                            <h5 className="team-name">{game.home.school}</h5>
+                        </div>
                     </div>
-                    <div className="vs">vs</div>
-                    <div className="team-info text-center">
-                        <img className="team-logo" src={game.home.logos[0]} alt="Home Team Logo" />
-                        <h5 className="team-name">{game.home.abbreviation}</h5>
-                    </div>
-                </div>
+                ))}
             </div>
         </div>
     );
