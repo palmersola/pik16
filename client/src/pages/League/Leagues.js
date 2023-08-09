db8b4b35911324e3a71ac9edfa79bdbea4cd5b44
Stephen Alfa <stojalfa@gmail.com>
2023-08-09 10:29:09 -0400

    Minor changes

--- client/src/pages/League/Leagues.js
+++ client/src/pages/League/Leagues.js
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 const Leagues = ({user, setLeagueId}) => {
     const [leagues, setLeagues] = useState([]);
+    const navigate = useNavigate()
+
+    const handleAddToLeague = (leagueId) => {
+        setLeagueId(leagueId); // Set the selected league ID
+        navigate('/'); // Navigate to the GameCard route
+    };
 
     useEffect(() => {
         axios.get('http://localhost:8080/api/league').then((response) => {
@@ -18,7 +24,7 @@ const Leagues = ({user, setLeagueId}) => {
                 {leagues.map((league) => (
                     <li key={league.id}>
                         <NavLink to={`/league/${league.leagueId}`}>{league.leagueName}</NavLink>
-                        <button onClick={() => setLeagueId(league.leagueId)}>Add to League</button>
+                        <button className="btn btn-primary" onClick={() => handleAddToLeague(league.leagueId)}>Add to League</button>
                     </li>
                 ))}
             </ul>
