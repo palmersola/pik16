037cffeeebeffb412f2109644e4f8d608ed8f72d
Ahmad Khan <ahmad.d.khan40@gmail.com>
2023-08-14 09:39:17 -0400

    gamecard

--- client/src/pages/GameCard/GameCard.js
+++ client/src/pages/GameCard/GameCard.js
@@ -50,12 +50,12 @@ const GameCard = ({user, selectedLeagueId}) => {
             <h1>Game Feed</h1>
             <div className="row">
                 {gamesArr.map((game, index) => (
-                    <div key={index} id={game.game.id} className="card m-3 p-3 col-sm-5 d-flex flex-row justify-content-between align-items-center">
+                    <div key={index} id={game.game.id} className="card m-3 p-3 col-sm-5 d-flex flex-row justify-content-between align-items-center game-card">
                         <div className="team-info text-center">
                             <img className="team-logo" src={game.away.logos[0]} alt="Away Team Logo" />
                             <h5 className="team-name">{game.away.school}</h5>
                         </div>
-                        <div className="vs">vs</div>
+                        <div className="vs"><h4>VS</h4></div>
                         <div className="team-info text-center">
                             <img className="team-logo" src={game.home.logos[0]} alt="Home Team Logo" />
                             <h5 className="team-name">{game.home.school}</h5>
